e4c8ca09f95cc311e30a3d1df6e63477198480b3
ginger59620 <a59620@alunos.ipb.pt>
2025-11-18 12:48:07 +0000

    The GOAP planner part 1

--- Assets/WorldStates.cs
+++ Assets/WorldStates.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,11 @@ public class WorldState
 {
     public string key;
     public int value;
+
+    internal Dictionary<string, int> GetStates()
+    {
+        throw new NotImplementedException();
+    }
 }
 public class WorldStates
 {
